6297076b2029cdc1be57f1e57c42d504b8ab7501
Ramagiri Nihal <nihalramagiri@gmail.com>
2024-10-08 11:36:52 +0530

    deploy to aws ec2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '34.228.235.216:3000' // Update this with your Node.js API base URL
+  apiUrl: 'http://34.228.235.216:3000' // Update this with your Node.js API base URL
 };
